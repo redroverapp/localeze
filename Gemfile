f4843c83f5b49045efcbd38855c676d42da81bcd
Roderick Monje <rod@seologic.com>
2011-11-16 13:54:08 -0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'bson_ext'
+gem 'jquery-rails', '>= 1.0.3'
+gem 'mongoid'
+gem 'rails', '3.0.10'
+gem 'scaffold_logic'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localeze::Application.config.secret_token = '6f6dfcc4ccd7a9bf7c529bb7204689f03846e15847ea70396979b984d73b3a2b0e0d4b10db445fb46b2e32bf16690a76454dfa1318cfd8e606709da5847a2a94'
