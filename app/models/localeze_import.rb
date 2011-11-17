c0dbecf3d5860a118bedd86a2dacf013c2241140
Roderick Monje <rod@seologic.com>
2011-11-17 12:43:22 -0600

    added import method to wrap each table import

--- app/models/localeze_import.rb
+++ app/models/localeze_import.rb
@@ -1,4 +1,32 @@
 class LocalezeImport < ScaffoldLogic::DataImport
+  MODELS_TO_FILES = {
+    BaseRecordImport => 'baserecords.txt',
+    CategoryImport => 'categories.txt'
+#     Chain => 'chains.txt',
+#     CompanyAttribute => 'companyattributes.txt',
+#     CompanyHeading => 'companyheadings.txt',
+#     CompanyPaymentType => 'companypaymenttypes.txt',
+#     CompanyPhone => 'companyphones.txt',
+#     CompanySic => 'companysic.txt',
+#     CompanyUnstructured => 'companyunstructured.txt',
+#     CondensedHeadingDetail => 'condensedheadingdetail.txt',
+#     CustomAttribute => 'customattributes.txt',
+#     CustomAttributeType => 'customattributetypes.txt',
+#     Language => 'languages.txt',
+#     NormalizedHeadingDetail => 'normalizedheadingdetail.txt',
+#     PaymentType => 'paymenttypes.txt',
+#     SicDetail => 'sicdetail.txt'
+  }.freeze
+
+  def self.import dir
+    MODELS_TO_FILES.inject({}) do |h, (import_model, file)|
+      import_model.test "db/#{dir}/Sample/#{file}"
+      model = Kernel.const_get(import_model.name.gsub 'Import', '')
+      h[model] = model.count
+      h
+    end
+  end
+
   def self.test path
     text = File.read path
     File.open(path, 'w') {|file| file.puts text.gsub(/"/, %{&quot;})}
