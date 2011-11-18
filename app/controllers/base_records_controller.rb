f7dae27c6bf8776c5ee2587f9dbf8d4c6397118b
Roderick Monje <rod@seologic.com>
2011-11-18 17:33:19 -0600

    added company-attributes to base-record show

--- app/controllers/base_records_controller.rb
+++ app/controllers/base_records_controller.rb
@@ -4,10 +4,11 @@ class BaseRecordsController < ApplicationController
   # CRUD ===========================================================================================
   def index
     @base_records = BaseRecord.order_by ScaffoldLogic.mongoid_sorter_from(params[:by], params[:dir])
-    @fields = LocalezeImport::MODEL_ATTRIBUTES[BaseRecord][:fields][0..15]
+    @base_record_fields = LocalezeImport::MODEL_ATTRIBUTES[BaseRecord][:fields][0..15]
   end
 
   def show
     @base_record = BaseRecord.find params[:id]
+    @company_attribute_fields = LocalezeImport::MODEL_ATTRIBUTES[CompanyAttribute][:fields]
   end
 end
--- app/models/base_record.rb
+++ app/models/base_record.rb
@@ -1,4 +1,36 @@
 class BaseRecord
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  def company_attributes
+    CompanyAttribute.where :pid => self.pid
+  end
+
+  def company_headings
+    CompanyHeading.where :pid => self.pid
+  end
+
+  def company_languages
+    CompanyLanguage.where :pid => self.pid
+  end
+
+  def company_payment_types
+    CompanyPaymentType.where :pid => self.pid
+  end
+
+  def company_phones
+    CompanyPhone.where :pid => self.pid
+  end
+
+  def company_sics
+    CompanySic.where :pid => self.pid
+  end
+
+  def company_unstructureds
+    CompanyUnstructured.where :pid => self.pid
+  end
+
+  def custom_attributes
+    CustomAttribute.where :pid => self.pid
+  end
 end
