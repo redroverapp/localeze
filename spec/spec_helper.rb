59811840b0a9ae316b483135c4a42cec7d16b64d
Roderick Monje <rod@seologic.com>
2011-12-02 15:04:06 -0600

    pruned extra lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.mock_with :mocha
 end
-
 def login_admin
   @admin = User.first(:conditions => {:email => 'someone@seologic.com'}) || User.make(:email => 'someone@seologic.com')
   sign_in @admin
